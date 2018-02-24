808b291eeb2cb92aff529f404d77fe50eb226b82
David Margolin <davidmargolin0305@gmail.com>
2018-02-24 12:13:30 -0500

    fix kotlin error

--- app/src/main/java/com/iter/marmoset/MainActivity.kt
+++ app/src/main/java/com/iter/marmoset/MainActivity.kt
@@ -1,3 +1,5 @@
+package com.iter.marmoset
+
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
@@ -5,21 +7,19 @@ import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import com.firebase.ui.auth.AuthUI
+import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
-import com.iter.marmoset.R
-import com.iter.marmoset.SalonFragment
-
 
 // sign in response id and providers used with AuthUI
 private const val RC_SIGN_IN = 420
 private val providers = mutableListOf(
         AuthUI.IdpConfig.EmailBuilder().build())
-class MainActivity : AppCompatActivity(){
+
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        FirebaseApp.initializeApp(this)
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
-
         val user = FirebaseAuth.getInstance().currentUser
         if (user != null) {
             // user already signed in
@@ -45,7 +45,7 @@ class MainActivity : AppCompatActivity(){
         super.onActivityResult(requestCode, resultCode, data)
 
         if (requestCode == RC_SIGN_IN) {
-            if (resultCode == RESULT_OK) {
+            if (resultCode == AppCompatActivity.RESULT_OK) {
                 // Successfully signed
                 val vaccineFragment: Fragment = SalonFragment.newInstance(1)
                 supportFragmentManager.beginTransaction().replace(R.id.content_view, vaccineFragment).commit()
