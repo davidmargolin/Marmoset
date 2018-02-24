e8c2a3de247d5fa4f7594c74fb38ea38d705be6c
David Margolin <davidmargolin0305@gmail.com>
2018-02-24 16:32:58 -0500

    billing info holder

--- app/src/main/java/com/iter/marmoset/SalonActivity.java
+++ app/src/main/java/com/iter/marmoset/SalonActivity.java
@@ -1,5 +1,7 @@
 package com.iter.marmoset;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,7 +10,6 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 
-import com.daimajia.slider.library.Indicators.PagerIndicator;
 import com.daimajia.slider.library.SliderLayout;
 import com.daimajia.slider.library.SliderTypes.BaseSliderView;
 import com.daimajia.slider.library.SliderTypes.TextSliderView;
@@ -16,7 +17,7 @@ import com.daimajia.slider.library.SliderTypes.TextSliderView;
 import java.util.ArrayList;
 
 public class SalonActivity extends AppCompatActivity {
-
+    String name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +25,8 @@ public class SalonActivity extends AppCompatActivity {
         setContentView(R.layout.activity_salon);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        name = getIntent().getStringExtra("name");
+        getSupportActionBar().setTitle(name);
         ArrayList<String> slideshow = getIntent().getStringArrayListExtra("slideshow");
         Log.e("urls", slideshow.toString());
         SliderLayout sliderLayout = findViewById(R.id.promo_slider);
@@ -38,8 +41,10 @@ public class SalonActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(getApplication(), BillingActivity.class);
+                intent.putExtra("id", 529852);
+                intent.putExtra("name", name);
+                startActivity(intent);
             }
         });
     }
