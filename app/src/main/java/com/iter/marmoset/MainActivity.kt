c779a8ee35afa280c9a03ea9d242384a7d49df5a
David Margolin <davidmargolin0305@gmail.com>
2018-02-24 16:58:02 -0500

    added navigation bottom bar view

--- app/src/main/java/com/iter/marmoset/MainActivity.kt
+++ app/src/main/java/com/iter/marmoset/MainActivity.kt
@@ -2,13 +2,13 @@ package com.iter.marmoset
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import com.firebase.ui.auth.AuthUI
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
 
 // sign in response id and providers used with AuthUI
 private const val RC_SIGN_IN = 420
@@ -16,15 +16,19 @@ private val providers = mutableListOf(
         AuthUI.IdpConfig.EmailBuilder().build())
 
 class MainActivity : AppCompatActivity(), SalonFragment.OnListFragmentInteractionListener{
+    var bottomnav : BottomNavigationView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         FirebaseApp.initializeApp(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // setup bottom navigation bar
+        bottomnav = navigation
+        // bottomnav!!.visibility = View.GONE
+        bottomnav!!.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
         val user = FirebaseAuth.getInstance().currentUser
         if (user != null) {
             // user already signed in
-            val salonFragment: Fragment = SalonFragment.newInstance()
-            supportFragmentManager.beginTransaction().replace(R.id.content_view, salonFragment).commit()
+            bottomnav!!.selectedItemId = R.id.salons
 
         }else{
             // handle sign in
@@ -40,19 +44,40 @@ class MainActivity : AppCompatActivity(), SalonFragment.OnListFragmentInteractio
 
     }
 
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+        // TODO: decide on which navigation menus to include
+            R.id.salons -> {
+                val salonFragment: Fragment = SalonFragment.newInstance()
+                supportFragmentManager.beginTransaction().replace(R.id.content_view, salonFragment).commit()
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.appointments -> {
+                val salonFragment: Fragment = SalonFragment.newInstance()
+                supportFragmentManager.beginTransaction().replace(R.id.content_view, salonFragment).commit()
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.profile -> {
+                val salonFragment: Fragment = SalonFragment.newInstance()
+                supportFragmentManager.beginTransaction().replace(R.id.content_view, salonFragment).commit()
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
         if (requestCode == RC_SIGN_IN) {
-            if (resultCode == AppCompatActivity.RESULT_OK) {
-                // Successfully signed
-                val vaccineFragment: Fragment = SalonFragment.newInstance()
-                supportFragmentManager.beginTransaction().replace(R.id.content_view, vaccineFragment).commit()
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in - update vaccines
+                if (bottomnav != null){
+                    bottomnav!!.selectedItemId = R.id.salons
+                }
             } else {
                 // TODO: handle sign in failed
             }
         }
+        bottomnav!!.selectedItemId = R.id.salons
 
     }
 
@@ -66,23 +91,4 @@ class MainActivity : AppCompatActivity(), SalonFragment.OnListFragmentInteractio
         startActivity(intent)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-//        val inflater : MenuInflater = getMenuInflater()
-//        inflater.inflate(R.menu.main,menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-//        when(item!!.itemId){
-//            R.id.add -> {
-//                val newAdder = Intent(this, AddVaccination::class.java)
-//                startActivityForResult(newAdder, 5)
-//            }
-//            else -> {
-//                return true
-//            }
-//
-//        }
-        return super.onOptionsItemSelected(item)
-    }
 }
